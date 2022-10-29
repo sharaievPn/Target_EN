890604ddaaf4e98c43f255cd05b3385cb344b503
Vitalii <sharaiev.pn@ucu.edu.ua>
2022-10-29 16:12:54 +0300

    Corrected mistakes in checking for correct words in dictionary. Added dictionary to dictionary.txt

--- target_game.py
+++ target_game.py
@@ -6,7 +6,7 @@ import random
 def generate_grid() -> List[str]:
     """
     Generates list of lists of letters - i.e. grid for the game.
-    e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
+    e.g. ['i', 'e', 'a', 'd', 'c', 'b', 'g', 'a', 'k']
     """
     board = random.choices(string.ascii_lowercase, k=9)
     return board
@@ -23,27 +23,32 @@ def get_words(f: str, letters: List[str]) -> List[str]:
     tuples = []
     central_letter = letters[4]
     while len(letters) != 0:
-        tuples.append((letters[0], letters.count(letters[0])))
-        letters.remove(letters[0])
+        char = letters[0]
+        tuples.append((letters[0], letters.count(char)))
+        for i in range(letters.count(char)):
+            letters.remove(char)
 
     for word in dictionary:
+
         possible_word = word
+
         if len(word) > 9 or central_letter not in word:
             continue
-        flag = True
-        while len(tuples) != 0:
-            if word.count(tuples[0][0]) == -1:
-                continue
-            if word.count(tuples[0][0]) > tuples[0][1]:
-                flag = False
-                break
-            word.replace(tuples[0][0], '')
-            tuples.remove(tuples[0])
-        if not flag:
-            break
 
-        if len(word) == 0:
-            acceptable_words.append(possible_word)
+        for k in range(len(tuples)):
+            if possible_word.count(tuples[k][0]) == 0:
+                continue
+            elif possible_word.count(tuples[k][0]) > tuples[k][1]:
+                break
+            else:
+                possible_word = possible_word.replace(tuples[k][0], '')
+
+        if len(possible_word) == 0:
+            try:
+                if acceptable_words.index(word) > 0:
+                    continue
+            except:
+                acceptable_words.append(word)
 
     return acceptable_words
 
@@ -64,6 +69,9 @@ def get_user_words() -> List[str]:
             word = input()
         except:
             running = False
+
+        if not word:
+            break
 
         try:
             if user_words.index(word) >= 0:
@@ -88,9 +96,10 @@ def get_pure_user_words(user_words: List[str], letters: List[str], words_from_di
                 correct_words.append(word)
                 user_words.remove(word)
         except:
+            no_such_word.append(word)
             user_words.remove(word)
 
-    return correct_words
+    return no_such_word
 
 
 def results():
